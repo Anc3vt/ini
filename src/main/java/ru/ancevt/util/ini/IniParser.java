b45bb77018c27603f0c20d9aaab63043236c9672
ancevt <i@ancevt.ru>
2019-12-03 22:14:04 +0300

    Safe commit

--- src/main/java/ru/ancevt/util/ini/IniParser.java
+++ src/main/java/ru/ancevt/util/ini/IniParser.java
@@ -1,97 +1,96 @@
 package ru.ancevt.util.ini;
 
 public final class IniParser {
-	
-	public static Ini createIni(String iniData) {
-		final IniParser iniParser = new IniParser();
-		return iniParser.parse(iniData);
-	}
 
-	private Ini ini;
-	private String sourceData;
-	private IniSection section;
+    public static Ini createIni(String iniData) {
+        final IniParser iniParser = new IniParser();
+        return iniParser.parse(iniData);
+    }
 
-	public IniParser() {
+    private Ini ini;
+    private String sourceData;
+    private IniSection section;
 
-	}
+    public IniParser() {
 
-	public Ini parse(Ini ini, String iniData) {
-		this.ini = ini;
-		this.sourceData = iniData;
-		actualParse();
-		return ini;
-	}
+    }
 
-	public Ini parse(String iniData) {
-		return parse(new Ini(), iniData);
-	}
+    public Ini parse(Ini ini, String iniData) {
+        this.ini = ini;
+        this.sourceData = iniData;
+        actualParse();
+        return ini;
+    }
 
-	private final void actualParse() {
-		final String delimiter = Character.toString(IniChar.END_LINE);
-		final String[] lines = sourceData.split(delimiter);
+    public Ini parse(String iniData) {
+        return parse(new Ini(), iniData);
+    }
 
-		section = ini.getGlobalSection();
+    private final void actualParse() {
+        final String delimiter = Character.toString(IniChar.END_LINE);
+        final String[] lines = sourceData.split(delimiter);
 
-		for (final String l : lines) {
-			if(isLineEmpty(l)) {
-				section.addLine(new IniLine());
-				continue;
-			}
-			
-			final String trimmed = l.trim();
-			final char firstChar = trimmed.charAt(0);
+        section = ini.getGlobalSection();
 
-			switch (firstChar) {
-			case IniChar.COMMENT1:
-			case IniChar.COMMENT2:
-				parseCommentLine(l);
-				break;
-			case IniChar.OPEN_BRACE:
-				parseSectionLine(l);
-				break;
-			default:
-				parseLine(l);
-				break;
-			}
+        for (final String l : lines) {
+            if (isLineEmpty(l)) {
+                section.addLine(new IniLine());
+                continue;
+            }
 
-		}
+            final String trimmed = l.trim();
+            final char firstChar = trimmed.charAt(0);
 
-	}
+            switch (firstChar) {
+                case IniChar.COMMENT1:
+                case IniChar.COMMENT2:
+                    parseCommentLine(l);
+                    break;
+                case IniChar.OPEN_BRACE:
+                    parseSectionLine(l);
+                    break;
+                default:
+                    parseLine(l);
+                    break;
+            }
 
-	private static final boolean isLineEmpty(String line) {
-		return line.trim().length() == 0;
-	}
+        }
 
-	private final void parseLine(String line) {
-		final String delimiter = Character.toString(IniChar.EQUALS); // =
+    }
 
-		final IniLine iniLine = new IniLine();
+    private static final boolean isLineEmpty(String line) {
+        return line.trim().length() == 0;
+    }
 
-		if (!isLineEmpty(line) && !line.contains(delimiter)) {
-			throw new IniException(String.format("There is no '=' char in line \"%s\"", line));
-		} else {
-			final String[] s = line.split(delimiter);
-			final String left = s[0].trim();
-			final String right = s[1];//.trim();
-			iniLine.setKey(left);
-			iniLine.setValue(right);
-		}
+    private final void parseLine(String line) {
+        final String delimiter = Character.toString(IniChar.EQUALS); // =
 
-		section.addLine(iniLine);
-	}
+        final IniLine iniLine = new IniLine();
 
-	private final void parseCommentLine(String line) {
-		final String commentText = line.trim().substring(1).trim();
-		final IniLine iniLine = new IniLine(commentText);
-		section.addLine(iniLine);
-	}
+        if (!isLineEmpty(line) && !line.contains(delimiter)) {
+            throw new IniException(String.format("There is no '=' char in line \"%s\"", line));
+        } else {
+            final String[] s = line.split(delimiter);
+            final String left = s[0].trim();
+            final String right = s.length > 1 ? s[1] : "";//.trim();
+            iniLine.setKey(left);
+            iniLine.setValue(right);
+        }
 
-	private final void parseSectionLine(String line) {
-		line = line.trim();
-		final String sectionName = line.substring(1, line.length() - 1);
-		section = new IniSection(sectionName);
-		ini.addSection(section);
-	}
+        section.addLine(iniLine);
+    }
+
+    private final void parseCommentLine(String line) {
+        final String commentText = line.trim().substring(1).trim();
+        final IniLine iniLine = new IniLine(commentText);
+        section.addLine(iniLine);
+    }
+
+    private final void parseSectionLine(String line) {
+        line = line.trim();
+        final String sectionName = line.substring(1, line.length() - 1);
+        section = new IniSection(sectionName);
+        ini.addSection(section);
+    }
 
 }
-
